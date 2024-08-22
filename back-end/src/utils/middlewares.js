76f168a5181447eaee299cde7022048eeb7bb819
Juan Ramirez <ciscojuan@gmail.com>
2024-08-22 07:37:20 -0500

    path to private key changed to allow deploy on render

--- back-end/src/utils/middlewares.js
+++ back-end/src/utils/middlewares.js
@@ -1,30 +1,33 @@
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
-const public_key = fs.readFileSync("/home/makarov/Documents/projects/React/medic-me/back-end/src/utils/keys/public.pem");
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+// Resuelve la ruta absoluta del archivo private.pem
+const publicKeyPath = path.resolve(__dirname, '../utils/keys/private.pem');
+const public_key = fs.readFileSync(publicKeyPath);
 
 const auth = (req, res, next) => {
-    try {
-        const { authorization } = req.headers;
-        if (!authorization) {
-            throw new Error("Authorization header missing");
-        }
-        // Separate Bearer
-        const [, token] = authorization.split(" ");
-        console.log("Token received:", token); // Print the token
-        const payload = jwt.verify(token, public_key);
-
-        console.log(payload);
-        req.user = payload;
-        next();
-    } catch (error) {
-        console.error("Token verification failed:", error.message);
-        res.status(401).json({ message: "Unauthorized!" });
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw new Error('Authorization header missing');
     }
+    // Separate Bearer
+    const [, token] = authorization.split(' ');
+    console.log('Token received:', token); // Print the token
+    const payload = jwt.verify(token, public_key);
+
+    console.log(payload);
+    req.user = payload;
+    next();
+  } catch (error) {
+    console.error('Token verification failed:', error.message);
+    res.status(401).json({ message: 'Unauthorized!' });
+  }
 };
 
 module.exports = {
-/*     requestLogger,
+  /*     requestLogger,
     unknownEndpoint,
     errorHandler, */
-    auth
+  auth,
 };

f89dad2c4f08b513f379de02d06d6337f65bf099
Juan Ramirez <ciscojuan@gmail.com>
2024-08-22 19:06:18 -0500

    add save location feature

---
+++ front-end/src/components/admin/location.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import userService from "../../services/user";
+import Notification from "../../shared/Notification";
+
+const Location = ({ sedes, setSedes }) => {
+  const [especialidades, setEspecialidades] = useState([]);
+  const [name, setName] = useState("");
+  const [direction, setDirection] = useState("");
+  const [specialty, setSpecialty] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    userService.getSpecialty().then((res) => {
+      setEspecialidades(res.data);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newLocation = {
+      name: name,
+      direction: direction,
+      specialty: specialty,
+    };
+
+    try {
+      userService.savedLocation(newLocation).then((res) => {
+        console.log(res.data);
+        setSedes(sedes)
+        setTimeout(() => {
+          setMessage("Sede añadida");
+          setName("");
+          setDirection("");
+          setSpecialty("");
+        }, 1000);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form-control">
+      <div className="form-group">
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-control"
+          onChange={({ target }) => setName(target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="direccion">Direccion:</label>
+        <input
+          name="direccion"
+          id="direccion"
+          type="text"
+          className="form-control"
+          onChange={({ target }) => setDirection(target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Especialidad:</label>
+        <select
+          name="especialidad"
+          id="especialidad"
+          className="custom-select"
+          onChange={({ target }) => setSpecialty(target.value)}
+        >
+          {especialidades.map((especialidad) => (
+            <option value={especialidad._id} key={especialidad._id}>
+              {especialidad.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="form-group">
+        <button type="submit" className="btn btn-primary">
+          Guardar
+        </button>
+      </div>
+
+      <Notification message={message} />
+    </form>
+  );
+};
+
+export default Location;
